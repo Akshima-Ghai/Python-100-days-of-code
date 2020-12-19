bb63b8bdb1148ec61af2de320868a8a57db5aa9f
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2020-12-19 23:46:08 +0530

    Update day2-1.py

--- day2-1.py
+++ day2-1.py
@@ -1,5 +1,17 @@
-num = input("Enter two digit number")
-sum = int(num[0])+int(num[1])
-print(sum)
+# print("Welcome to the Band Name Generator")
+# city=input("Which city u grew up in?\n")
+# pet =input("What's your pet name?\n")
+# print("Your brand name could be "+city+" "+pet)
 
-print(3*3+3/3-3)
+# num = input("Enter two digit number")
+# sum = int(num[0])+int(num[1])
+# print(sum)
+#
+# print(3*3+3/3-3)
+
+#BMI CALCULATOR
+height = float(input("Enter height in m"))
+weight = float(input("enter weight in kg"))
+
+bmi = weight/(height**2)
+print(int(bmi))
