5d974230112adf2004fa14cd89220530fd17b337
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2020-12-21 22:02:44 +0530

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,12 +1,11 @@
 #TIP CALCULATOR
 
 print("Welcome to tip calculator")
-
-#TAKE INPUT 
+#TAKE INPUT
 bill = float(input("What was the total bill? "))
 tip = float(input("What percent tip would you like to give ? 10,12 or 15? "))
 people = int(input("How many people to split the bill? "))
 
 #CALCULATE
-individual_amt = (bill*(1+tip/100))/people
+individual_amt = round(bill*(1+tip/100)/people,2)
 print("Each person should pay: "+str(individual_amt))
