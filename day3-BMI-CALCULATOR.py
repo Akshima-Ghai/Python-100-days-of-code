0c82919e3204b82730701312c481e02519d03a4a
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2020-12-22 11:13:11 +0530

    Create day3-BMI-CALCULATOR.py

---
+++ day3-BMI-CALCULATOR.py
@@ -0,0 +1,14 @@
+#BMI Calculator with warning
+#
+# height = float(input("enter height in m"))
+# weight = float(input("Enter weight"))
+#
+# bmi = round(weight/height**2,2)
+# if bmi<18.5:
+#     print(f"{bmi} , u r underweight")
+# elif bmi<=25:
+#     print(f"{bmi}, u have a normal  weight")
+# elif bmi<=30:
+#     print(f"{bmi} , u r obese")
+# else:
+#     print(f"{bmi} , u r clinically obese")
