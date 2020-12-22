44d26167ab62acde127adeb89f61b5389362ed3a
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2020-12-22 11:14:37 +0530

    Create day3-LEAP-YEAR.py

---
+++ day3-LEAP-YEAR.py
@@ -0,0 +1,12 @@
+#LEAP YEAR
+# year = int(input("enter year to check if it is leap year or not "))
+# if year%4==0:
+#     if year%100==0:
+#         if year%400==0:
+#             print("it is a leap year")
+#         else:
+#             print("it is not a leap year")
+#     else:
+#         print("it is a leap  year")
+# else:
+#     print("it is not a leap year")
