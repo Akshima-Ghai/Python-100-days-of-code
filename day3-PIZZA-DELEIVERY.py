b774c580207378d093998ea57564253a037b0eaa
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2020-12-22 11:13:55 +0530

    Create day3-PIZZA-DELEIVERY.py

---
+++ day3-PIZZA-DELEIVERY.py
@@ -0,0 +1,25 @@
+#PIZZA DELEIVERY
+# print("Welcome to Python Pizza Deleveries")
+# size = input("What size Pizza do u want ? S,M,L ? ")
+# add_pepperoni = input("Do u want pepperoni ? Y or N")
+# extra_cheese = input("Do u want extra cheese ? Y or N")
+# price = 0
+#
+# if size=='S':
+#     price+=15
+# elif size == 'M':
+#     price+=20
+# elif size == 'L':
+#     price+=25
+# else:
+#     print("Select Correct Option")
+#
+# if add_pepperoni == 'Y':
+#     if size =='S':
+#         price+=2
+#     else:
+#         price+=3
+# if extra_cheese == 'Y':
+#     price+=1
+#
+# print(f"The total price of pizza is {price}")
