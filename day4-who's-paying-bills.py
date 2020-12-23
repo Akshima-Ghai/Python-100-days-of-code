713e0600d292f6fb10fe5d1cd541c06eb6a0a0fc
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2020-12-23 14:03:42 +0530

    Update day4-who's-paying-bills.py

--- day4-who's-paying-bills.py
+++ day4-who's-paying-bills.py
@@ -1,8 +1,16 @@
 # WHO'S PAYING BILLS
-import random
+# import random
+#
+# names = input("Enter names seperated by comma. ")
+# name_list = names.split(",")
+# n = len(name_list)
+# index = random.randint(0, n - 1)
+# print(f"{name_list[index]} will pay the bill")
 
-names = input("Enter names seperated by comma. ")
+# OR
+
+import random
+names = input("Enter name seperated by comma. ")
 name_list = names.split(",")
-n = len(name_list)
-index = random.randint(0, n - 1)
-print(f"{name_list[index]} will pay the bill")
+person_who_will_pay_bill = random.choice(name_list)
+print(f"{person_who_will_pay_bill} will pay the bill")
