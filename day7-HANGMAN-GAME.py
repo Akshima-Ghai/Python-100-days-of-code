e17be0611eae74e17a91b8d59bd117199561c566
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2020-12-25 22:24:40 +0530

    Update day7-HANGMAN-GAME.py

--- day7-HANGMAN-GAME.py
+++ day7-HANGMAN-GAME.py
@@ -6,3 +6,12 @@ chosen_word = random.choice(word_list)
 print(chosen_word)
 for j in range(len(chosen_word)):
     display.append("_")
+
+guess = input("Guess a letter : ").lower()
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
+    if letter==guess:
+        display[position] = letter
+
+
+print(display)
