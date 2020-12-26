60162b60a044bd2a51b1656d328938ab3b40a1a8
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2020-12-26 10:29:34 +0530

    Update day7-HANGMAN-GAME.py

--- day7-HANGMAN-GAME.py
+++ day7-HANGMAN-GAME.py
@@ -1,17 +1,40 @@
-#HANGMAN
+# HANGMAN
+
 import random
+import sys
+import hangman_design
+import hangman_word
+
+print(hangman_design.logo)
 display = []
-word_list = ["ardvark","baboon","camel"]
-chosen_word = random.choice(word_list)
-print(chosen_word)
+lives = 6
+
+chosen_word = random.choice(hangman_word.word_list)
+
 for j in range(len(chosen_word)):
     display.append("_")
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter : ").lower()
 
-guess = input("Guess a letter : ").lower()
-for position in range(len(chosen_word)):
-    letter = chosen_word[position]
-    if letter==guess:
-        display[position] = letter
+    if guess in display:
+        print(f"You've already guessed {guess}")
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
 
+    if guess not in chosen_word:
+        if lives == 0:
+            end_of_game = True
+            print("You Lose")
+            print(f"The correct word was {chosen_word}")
 
-print(display)
+        else:
+            print(f"You guessed {guess} , that's not in a word . You lose a life. ")
+            lives -= 1
+            print(hangman_design.stages[lives])
+    print(display)
+    if "_" not in display:
+        end_of_game = True
+        print("You Win.")
